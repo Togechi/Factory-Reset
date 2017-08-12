40daaf2649fa3c9f83fa46570532e2378a30f6a6
Togechi <wearerarnotzip@gmail.com>
2017-08-12 12:17:05 +0800

    Projectile Disappear
    Projectiles are able to disappear after x time (for homing bullets)

---
+++ Factory Reset/Assets/ProjectileDisappear.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileDisappear : MonoBehaviour {
+    private float Timer;
+    public float disappearAfter = 4f;
+
+	// Use this for initialization
+	void Start () {
+        Timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Timer += Time.deltaTime;
+        if (Timer >= disappearAfter)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
